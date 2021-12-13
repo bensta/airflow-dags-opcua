857e33277038a0f43b06c478c4091a1449334919
Ben <email@example.com>
2021-12-13 16:53:12 +0100

    use local bash op"

--- dag.py
+++ dag.py
@@ -6,7 +6,7 @@ from providers.airlaunch.opcua.transfers.opcua_to_s3 import OPCUAToS3Operator
 from providers.airlaunch.opcua.transfers.opcua_to_wasb import OPCUAToWasbOperator
 
 from airflow.providers.microsoft.azure.operators.wasb_delete_blob import WasbDeleteBlobOperator
-
+from airflow.operators.bash import BashOperator
 
 default_args = {
     'owner': 'airflow',
@@ -25,42 +25,48 @@ with DAG(
     start_date=datetime(2021, 1, 1),
     catchup=False,
 ) as dag:
-    opS3 = OPCUAToS3Operator(
-        task_id="opcS3",
-        opcua_conn_id="opc",
-        opcua_node="ns=3;i=1003",
-        s3_bucket="afb",
-        aws_conn_id="s3",
-        opcua_startdate="2021-12-09 20:00:00",
-        opcua_enddate="2021-12-09 20:01:00",
-        opcua_numvalues=10,
-        upload_format="json",
-        s3_key="opc_file.json",
-        replace=True,
-        queue="local"
+#    opS3 = OPCUAToS3Operator(
+#        task_id="opcS3",
+#        opcua_conn_id="opc",
+#        opcua_node="ns=3;i=1003",
+#        s3_bucket="afb",
+#        aws_conn_id="s3",
+#        opcua_startdate="2021-12-09 20:00:00",
+#        opcua_enddate="2021-12-09 20:01:00",
+#        opcua_numvalues=10,
+#        upload_format="json",
+#        s3_key="opc_file.json",
+#        replace=True,
+#        queue="local"
+#    )
+
+    helloLocal = BashOperator(
+        task_id='hello',
+        bash_command="echo 'hello local'",
+        queue='local'
     )
 
-    deleteBlob = WasbDeleteBlobOperator(
-        task_id='deleteBlob',
-        wasb_conn_id="wasb",
-        container_name='test',
-        blob_name='opc_data.json',
-        ignore_if_missing=True
-    )
+#    deleteBlob = WasbDeleteBlobOperator(
+#        task_id='deleteBlob',
+#        wasb_conn_id="wasb",
+#        container_name='test',
+#        blob_name='opc_data.json',
+#        ignore_if_missing=True
+#    )
 
-    opWasb = OPCUAToWasbOperator(
-        task_id = "opcWasb",
-        opcua_conn_id="opc",
-        opcua_node="ns=3;i=1003",
-        wasb_container="test",
-        wasb_blob="opc_data.json",
-        wasb_conn_id="wasb",
-        upload_format="json",
-        opcua_startdate="2021-12-09 20:00:00",
-        opcua_enddate="2021-12-09 20:01:00",
-        opcua_numvalues=10,
-        queue="local"
-    )
+#    opWasb = OPCUAToWasbOperator(
+#        task_id = "opcWasb",
+#        opcua_conn_id="opc",
+#        opcua_node="ns=3;i=1003",
+#        wasb_container="test",
+#        wasb_blob="opc_data.json",
+#        wasb_conn_id="wasb",
+#        upload_format="json",
+#        opcua_startdate="2021-12-09 20:00:00",
+#        opcua_enddate="2021-12-09 20:01:00",
+#        opcua_numvalues=10,
+#        queue="local",
+#    )
 
     deleteBlob >> opWasb
 
