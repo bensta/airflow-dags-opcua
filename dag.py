a33c6b3f795118c38a30bcab941b77f08030bd1a
Ben <email@example.com>
2021-12-15 09:37:06 +0100

    isolate wasb task

--- dag.py
+++ dag.py
@@ -26,39 +26,14 @@ with DAG(
     start_date=datetime(2021, 1, 1),
     catchup=False,
 ) as dag:
-    opS3 = OPCUAToS3Operator(
-        task_id="opcS3",
-        opcua_conn_id="opc",
-        opcua_node="ns=3;i=1003",
-        s3_bucket="afb",
-        aws_conn_id="s3",
-        opcua_startdate="2021-12-09 20:00:00",
-        opcua_enddate="2021-12-09 20:01:00",
-        opcua_numvalues=10,
-        upload_format="json",
-        s3_key="opc_file.json",
-        replace=True,
-        queue="local"
-    )
 
-#    sleep_remote = BashOperator(
-#        task_id='sleep_remote',
-#        bash_command='sleep 300'
-#    )
-#
-#    sleep_local = BashOperator(
-#        task_id='sleep_local',
-#        bash_command='sleep 300',
-#        queue='local'
-#    )
-
-    deleteBlob = WasbDeleteBlobOperator(
-        task_id='deleteBlob',
-        wasb_conn_id="wasb",
-        container_name='test',
-        blob_name='opc_data.json',
-        ignore_if_missing=True
-    )
+    #deleteBlob = WasbDeleteBlobOperator(
+    #    task_id='deleteBlob',
+    #    wasb_conn_id="wasb",
+    #    container_name='test',
+    #    blob_name='opc_data.json',
+    #    ignore_if_missing=True
+    #)
 
     opWasb = OPCUAToWasbOperator(
         task_id = "opcWasb",
@@ -74,7 +49,7 @@ with DAG(
         queue="local",
     )
 
-    deleteBlob >> opWasb
+    #deleteBlob >> opWasb
 
 
     
